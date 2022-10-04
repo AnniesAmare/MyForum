7c80f1645a72ccfd98e98be3b9a918cab9daf45d
AnniesAmare <annethearendtorff@gmail.com>
2022-10-04 16:07:57 +0200

    Changing MyForumService and FetchData to include posts from other users

--- MyForum/Data/MyForumService.cs
+++ MyForum/Data/MyForumService.cs
@@ -24,5 +24,29 @@ namespace MyForum.Data
                  // Use ToListAsync to avoid blocking a thread
                  .AsNoTracking().ToListAsync();
         }
+        public async Task<List<Posts>>
+            GetOtherPostsAsync(string strCurrentUser)
+        {
+            // Get Posts
+            return await _context.Posts
+                 // Only get entries for the current logged in user
+                 .Where(x => x.UserName != strCurrentUser)
+                 // Use AsNoTracking to disable EF change tracking
+                 // Use ToListAsync to avoid blocking a thread
+                 .AsNoTracking().ToListAsync();
+        }
+
+
+        /* Function to get all posts
+                public async Task<List<Posts>>
+                    GetPostsAsync2()
+                {
+                    // Get Posts
+                    return await _context.Posts
+                         // Use AsNoTracking to disable EF change tracking
+                         // Use ToListAsync to avoid blocking a thread
+                         .AsNoTracking().ToListAsync();
+                }
+        */
     }
 }
